b820d4cdf5f6457c61f09e7546350a6b6888cf74
MyLoginHathBeenTaken <jtng75@gmail.com>
2024-02-15 05:48:33 +0000

    refactor to promises to enable code reuse

--- index.js
+++ index.js
@@ -1,72 +1,68 @@
 const fs = require ('node:fs');
 const path = require ('node:path');
-const { escape } = require('node:querystring');
-const { parseArgs } = require('node:util');
 const { EOL } = require("os");
 const util = require('util');
 
 module.exports = async function (confPath = './config.conf', autoflush = true) {
-    try {
-        const conf = await confToJson(confPath); // Await for confToJson to resolve
-        console.log(`conf path is: ${confPath}`);
-        console.log(util.inspect(conf, {depth: null}));
-        return {
-            getKey: (key) => {
-                return conf[key]
-            }, // Define functions inside the returned object
-            setKey: (key, value) => { 
-                conf[key] = value;
-                return conf[key]
-            }
-        };
-    } catch (err) {
-        console.error('Error loading configuration:', err);
-        throw err; // Re-throw to allow handling by the caller
-    }
+    return new Promise((resolve, reject) => {
+        try {
+            let conf = {}
+            confToJson(confPath).then(a => {
+                console.log(`conf path is: ${confPath}`);
+                console.log(util.inspect(a, { depth: null }));
+                let modExport = {
+                    getKey: (key) => {
+                        return a[key]
+                    }, // Define functions inside the returned object
+                    setKey: (key, value) => {
+                        a[key] = value;
+                        return a[key]
+                    }
+                }
+                resolve(modExport)
+            })
+        } catch (err) {
+            reject(err)
+        }
+    })
 };
 
 function confToJson(confPath) {
-    try {
+    return new Promise((resolve, reject) => {
         let ext = path.extname(confPath);
         if (ext == '.conf') {
-            fs.accessSync(confPath, fs.constants.R_OK | fs.constants.W_OK)
             try {
-                let confJson = readLines(confPath)
-                return confJson
+                fs.accessSync(confPath, fs.constants.R_OK | fs.constants.W_OK)
+                let confJson = {}
+                readLines(confPath).then(a => {
+                    console.log('test' + a)
+                    for (i in a) {
+                        let kvp = parser(a[i])
+                        confJson = {
+                            ...confJson,
+                            ...kvp
+                        }
+                    }
+                    resolve(confJson)
+                })
             } catch (err) {
-                throw err;
+                reject(err)
             }
-        } else {
-            throw 'invalidExt'
         }
-    } catch (err) {
-        return err
-    }
-}
+    })
+};
 
 function readLines(confPath) {
     return new Promise((resolve, reject) => {
-        let kvpObj = {};
+        let stringArray = [];
         const stream = fs.createReadStream(confPath, { encoding: 'utf8' });
         console.log(`reading ${confPath}`)
         stream.on('data', raw => {
             try {
-                
                 let strings = raw.split(EOL)
-                // for (i in strings)  {
-                //     strings[i] = strings[i].trim()
-                //     if (strings[i] === '') {
-                //         strings.pop()
-                //         i--
-                //     }
-                //     console.log(`line ${i} of strings: ${strings[i]}`)
-                // } 
                 for (i in strings)  {
-                    let kvp = parser(strings[i])
-                    kvpObj = {
-                        ...kvpObj,
-                        ...kvp
-                    }
+                    stringArray.push(strings[i].trim())
+                    console.log(`strings[i] in readline: ${strings[i]}`)
                 }
             } catch (err) {
                 reject(err)
@@ -75,20 +71,18 @@ function readLines(confPath) {
 
         stream.on('close', () => {
             console.log('closed')
-            console.log(util.inspect(kvpObj, { depth: null }))
-            resolve(kvpObj);
+            resolve(stringArray);
         })
         stream.on('error', err => {
             reject(err);
         });
 
         stream.on('end', () => {
-            console.log('end: ' + util.inspect(kvpObj, { depth: null }))
-            console.log('read done')
-        });
+            resolve(stringArray);
+        })
 
-    });
-}
+    })
+};
 
 function escaper(string) {
     let inString = false; // Track being inside single or double quotes
@@ -151,4 +145,9 @@ function parser(line) {
         }
     }
     return obj
-}
+};
+
+function pushChanges(conf) {
+    // find keys and update values and if there is a commet reattach coment to end
+    // append all new keys to eof
+};
